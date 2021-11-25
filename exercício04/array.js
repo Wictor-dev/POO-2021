e58d88605e7d290c0b8aa0148d64448507d44a9a
Wictor Gabriel <61809322+Wictor-dev@users.noreply.github.com>
2021-11-24 23:15:58 -0300

    Delete array.js

--- exercício04/array.js
+++
@@ -1,9 +0,0 @@
-function somarItems(arr) {
-    var result = 0;
-    for (var i = 0; i < arr.length; i++) {
-        result += arr[i];
-    }
-    return result;
-}
-var array = [1, 4, 66, 8, 10];
-console.log(somarItems(array))
